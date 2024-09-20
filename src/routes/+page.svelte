701bb4abb020c503375c37950ea14145bfe29340
Vikash Pramanik <vikashpramanikofficial@gmail.com>
2024-09-20 22:01:55 +0530

    Initial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+    export let data 
+
+</script>
+
+<p class= hover:>"font-bold padding:20 text-center"> {data.content}
+</p>
